42863f4b0b2300a520d08c5c858f3ff37091e987
SeongsoNoh <nsso0524@gmail.com>
2023-01-31 18:18:40 -0600

    test

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home({ results }) {
 }
 export async function getServerSideProps() {
   const { results } = await (
-    await fetch(`http://localhost:3000/api/movies`)
+    await fetch(`https://next-movie-pearl.vercel.app/api/movies`)
   ).json();
   return {
     props: {
